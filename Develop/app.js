c2d36a87129c637d94159601d57656d5ef7aeb60
Morgan <morganw118@gmail.com>
2020-12-01 23:06:13 -0800

    render and write to file function are working and new html is created

--- Develop/app.js
+++ Develop/app.js
@@ -6,7 +6,7 @@ const path = require("path");
 const fs = require("fs");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "./output/team.html");
+const outputPath = path.join(OUTPUT_DIR, "/team.html");
 
 const render = require("./lib/htmlRenderer");
 
@@ -40,7 +40,7 @@ const managerQs = [
         type: 'list',
         message: 'Which type of team member would you like to add?',
         name: 'type',
-        choices: ['Engineer', 'Intern', 'I dont want to add any more team members'],
+        choices: ['Manager','Engineer', 'Intern', 'I dont want to add any more team members'],
     },
 ]
 
@@ -69,7 +69,7 @@ const engineerQs = [
         type: 'list',
         message: 'Which type of team member would you like to add?',
         name: 'type',
-        choices: ['Engineer', 'Intern', 'I dont want to add any more team members'],
+        choices: ['Manager', 'Engineer', 'Intern', 'I dont want to add any more team members'],
     },
 ]
 
@@ -98,7 +98,7 @@ const internQs = [
         type: 'list',
         message: 'Which type of team member would you like to add?',
         name: 'type',
-        choices: ['Engineer', 'Intern', 'I dont want to add any more team members'],
+        choices: ['Manager', 'Engineer', 'Intern', 'I dont want to add any more team members'],
     },
 ]
 
@@ -107,14 +107,21 @@ function managerPrompt() {
         let manager = new Manager(res.name, res.id, res.email, res.officeNumber);
         employees.push(manager);
 
-        if (res.type === 'Engineer') {
+        if (res.type === 'Manager') {
+            managerPrompt();
+        }
+        else if (res.type === 'Engineer') {
             engineerPrompt();
         }
         else if(res.type === 'Intern') {
             internPrompt();
         }
-        else {
-            return;
+        else if (res.type === 'I dont want to add any more team members') {
+            const teamHtml = render(employees);
+            fs.writeFile(outputPath, teamHtml, (err) => {
+                if (err) throw err;
+                console.log('Team was successfully built!');
+            });
         }
     });
 }
@@ -124,14 +131,21 @@ function engineerPrompt() {
         let engineer = new Engineer(res.name, res.id, res.email, res.github);
         employees.push(engineer);
 
-        if (res.type === 'Intern') {
-            internPrompt();
-        }
-        else if (res.type === "Manager") {
+        if (res.type === 'Manager') {
             managerPrompt();
         }
-        else {
-            return;
+        else if (res.type === 'Engineer') {
+            engineerPrompt();
+        }
+        else if(res.type === 'Intern') {
+            internPrompt();
+        }
+        else if (res.type === 'I dont want to add any more team members') {
+            const teamHtml = render(employees);
+            fs.writeFile(outputPath, teamHtml, (err) => {
+                if (err) throw err;
+                console.log('Team was successfully built!');
+            });
         }
     });
 }
@@ -141,40 +155,23 @@ function internPrompt() {
         let intern = new Intern(res.name, res.id, res.email, res.school);
         employees.push(intern);
 
-        if (res.type === 'Engineer') {
-            engineerPrompt();
-        }
-        else if (res.type === "Manager") {
+        if (res.type === 'Manager') {
             managerPrompt();
         }
-        else {
-            return;
+        else if (res.type === 'Engineer') {
+            engineerPrompt();
+        }
+        else if(res.type === 'Intern') {
+            internPrompt();
+        }
+        else if (res.type === 'I dont want to add any more team members') {
+            const teamHtml = render(employees);
+            fs.writeFile(outputPath, teamHtml, (err) => {
+                if (err) throw err;
+                console.log('Team was successfully built!');
+            });
         }
     });
 }
 
 managerPrompt();
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-render(employees)
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-fs.writeFile('./output/team.html', render(data), (err) => {
-    if (err) throw err;
-    console.log('Team was successfully built!');
-});
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
